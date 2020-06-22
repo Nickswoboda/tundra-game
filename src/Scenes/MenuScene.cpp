957884ff9bac60181c284fea4a9adb5de12caba2
Nickswoboda <41607512+Nickswoboda@users.noreply.github.com>
2020-06-21 20:27:57 -0700

    add VContainer to menu scene

--- src/Scenes/MenuScene.cpp
+++ src/Scenes/MenuScene.cpp
@@ -15,6 +15,12 @@ MenuScene::MenuScene()
 	level_select_button_ = ui_layer_->AddButton({ 580, 190, 200, 32 }, "-Level Select", [&]() {manager_->PushScene(std::unique_ptr<Scene>(new LevelSelectScene())); });
 	options_button_ = ui_layer_->AddButton({ 580, 230, 200, 32 }, "-Options", [&]() {manager_->PushScene(std::unique_ptr<Scene>(new OptionsScene())); });
 	exit_button_ = ui_layer_->AddButton({ 580, 270, 200, 32 }, "-Quit", [&]() { Aegis::Application::Quit(); });
+
+	v_box_ = new Aegis::VContainer({0, 150, 1280, 570}, 10, Aegis::VContainer::Alignment::Left);
+	v_box_->AddWidget(new_game_button_);
+	v_box_->AddWidget(level_select_button_);
+	v_box_->AddWidget(options_button_);
+	v_box_->AddWidget(exit_button_);
 }
 
 MenuScene::~MenuScene()
