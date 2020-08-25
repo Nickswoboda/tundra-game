88606d710331e2306cefa09e219b9fb0c919dab6
Nick Swoboda <nickswoboda@gmail.com>
2020-08-24 22:09:12 -0700

    Add proper buttons to Menu

--- src/Scenes/MenuScene.cpp
+++ src/Scenes/MenuScene.cpp
@@ -11,25 +11,27 @@ MenuScene::MenuScene()
 
 	title_background_ = Aegis::TextureManager::Instance().Load("assets/textures/TundraBG.png");
 
-	auto title_text_texture = Aegis::Texture::TextureFromText("TUNDRA", title_font_);
-	title_sprite_ = std::make_shared<Aegis::Sprite>(Aegis::Vec2(0,50), title_text_texture->size_, title_text_texture);
+	auto title_texture = Aegis::Texture::Create("assets/textures/TundraTitle.png"); 
+	title_sprite_ = std::make_shared<Aegis::Sprite>(Aegis::Vec2(0,50), title_texture->size_, title_texture);
 	Aegis::CenterAABBHorizontally(title_sprite_->rect_, {0, 0, 1280, 720});
 	ui_layer_ = std::make_unique<Aegis::UILayer>();
 	ui_layer_->SetFont(button_font_);
-	new_game_button_ = ui_layer_->AddWidget<Aegis::Button>(new Aegis::Button({ 580, 150, 125, 50 }, "", [&]() {manager_->PushScene(std::unique_ptr<Scene>(new GameplayScene(0))); }));
-	level_select_button_ = ui_layer_->AddWidget<Aegis::Button>(new Aegis::Button({ 580, 190, 200, 32 }, "-Level Select", [&]() {manager_->PushScene(std::unique_ptr<Scene>(new LevelSelectScene())); }));
-	options_button_ = ui_layer_->AddWidget<Aegis::Button>(new Aegis::Button({ 580, 230, 200, 32 }, "-Options", [&]() {manager_->PushScene(std::unique_ptr<Scene>(new OptionsScene())); }));
-	exit_button_ = ui_layer_->AddWidget<Aegis::Button>(new Aegis::Button({ 580, 270, 200, 32 }, "-Quit", [&]() { Aegis::Application::Quit(); }));
+	new_game_button_ = ui_layer_->AddWidget<Aegis::Button>(new Aegis::Button({ 580, 170, 200, 50 }, "", [&]() {manager_->PushScene(std::unique_ptr<Scene>(new GameplayScene(0))); }));
+	level_select_button_ = ui_layer_->AddWidget<Aegis::Button>(new Aegis::Button({ 580, 220, 200, 50 }, "", [&]() {manager_->PushScene(std::unique_ptr<Scene>(new LevelSelectScene())); }));
+	options_button_ = ui_layer_->AddWidget<Aegis::Button>(new Aegis::Button({ 580, 230, 200, 50 }, "", [&]() {manager_->PushScene(std::unique_ptr<Scene>(new OptionsScene())); }));
+	exit_button_ = ui_layer_->AddWidget<Aegis::Button>(new Aegis::Button({ 580, 270, 200, 50 }, "", [&]() { Aegis::Application::Quit(); }));
 
-	v_box_ = ui_layer_->AddContainer({0, 150, 1280, 570}, Aegis::Container::Vertical, 10, Aegis::Container::Center);
+	v_box_ = ui_layer_->AddContainer({0, 170, 1280, 570}, Aegis::Container::Vertical, 10, Aegis::Container::Center);
 	v_box_->AddWidget(new_game_button_);
 	v_box_->AddWidget(level_select_button_);
 	v_box_->AddWidget(options_button_);
 	v_box_->AddWidget(exit_button_);
 	
-	new_game_button_->SetStateTexture(Aegis::Button::Normal, Aegis::TextureManager::Instance().Load("assets/textures/normalbtn.png"));
-	new_game_button_->SetStateTexture(Aegis::Button::Hovered, Aegis::TextureManager::Instance().Load("assets/textures/hoveredbtn.png"));
-	new_game_button_->SetStateTexture(Aegis::Button::Pressed, Aegis::TextureManager::Instance().Load("assets/textures/pressedbtn.png"));
+	new_game_button_->SetStateTexture(Aegis::Button::Normal, Aegis::TextureManager::Instance().Load("assets/textures/PlayButton.png"));
+	level_select_button_->SetStateTexture(Aegis::Button::Normal, Aegis::TextureManager::Instance().Load("assets/textures/LevelSelectButton.png"));
+	options_button_->SetStateTexture(Aegis::Button::Normal, Aegis::TextureManager::Instance().Load("assets/textures/OptionsButton.png"));
+	exit_button_->SetStateTexture(Aegis::Button::Normal, Aegis::TextureManager::Instance().Load("assets/textures/ExitButton.png"));
+	
 }
 
 MenuScene::~MenuScene()
