4d50ec7d3f6fe2be4f239c423b6edbaeed0509c6
Nick Swoboda <nickswoboda@gmail.com>
2021-03-08 21:07:28 -0800

    Polish: Improve look of ScoreBoard
    - Remove "Lives:" and "Time:" labels
    - Add more padding between widgets

--- src/ScoreBoard.cpp
+++ src/ScoreBoard.cpp
@@ -4,15 +4,14 @@ ScoreBoard::ScoreBoard(int num_lives, int num_pellets)
 {
 	auto sprite_sheet = Aegis::TextureManager::Load("assets/textures/tile_map.png");
 	bg_ = std::make_unique<Aegis::SpriteWidget>(Aegis::Vec2(20, 16), Aegis::TextureManager::Load("assets/textures/score_frame.png"));
-	v_box_ = std::make_unique<Aegis::Container>(Aegis::AABB(20, 16, 98, 140), Aegis::Container::Vertical, 4, Aegis::Alignment::Top | Aegis::Alignment::HCenter);
-	v_box_->AddWidget<Aegis::Label>("Lives:", Aegis::Vec2(), Aegis::Vec4(0,0,0,1));
-	auto heart_container = v_box_->AddWidget<Aegis::Container>(Aegis::AABB( 0, 0, 98, 20 ), Aegis::Container::Horizontal, 4, Aegis::Alignment::VCenter | Aegis::Alignment::HCenter);
+	v_box_ = std::make_unique<Aegis::Container>(Aegis::AABB(20, 16, 96, 128), Aegis::Container::Vertical, 8, Aegis::Alignment::Center);
+	auto heart_container = v_box_->AddWidget<Aegis::Container>(Aegis::AABB( 0, 0, 96, 16 ), Aegis::Container::Horizontal, 4, Aegis::Alignment::VCenter | Aegis::Alignment::HCenter);
 	for (int i = 0; i < num_lives; ++i) {
 		heart_widgets_[i] = heart_container->AddWidget<Aegis::SpriteWidget>(Aegis::Vec2(), sprite_sheet, Aegis::AABB(128, 112, 16, 16));
 	}
-	v_box_->AddWidget<Aegis::SpriteWidget>(Aegis::Vec2(), sprite_sheet, Aegis::AABB(96, 96, 32, 32));
-	pellet_count_label_ = v_box_->AddWidget<Aegis::Label>("0/" + std::to_string(num_pellets), Aegis::Vec2(), Aegis::Vec4(0, 0, 0, 1));
-	v_box_->AddWidget<Aegis::Label>("Time:", Aegis::Vec2(), Aegis::Vec4(0,0,0,1));
+	auto fish_container = v_box_->AddWidget<Aegis::Container>(Aegis::AABB(0, 0, 96, 55), Aegis::Container::Vertical, 2, Aegis::Alignment::Center);
+	fish_container->AddWidget<Aegis::SpriteWidget>(Aegis::Vec2(), sprite_sheet, Aegis::AABB(96, 96, 32, 32));
+	pellet_count_label_ = fish_container->AddWidget<Aegis::Label>("0/" + std::to_string(num_pellets), Aegis::Vec2(), Aegis::Vec4(0, 0, 0, 1));
 	time_label_ = v_box_->AddWidget<Aegis::Label>("00:00:00", Aegis::Vec2(), Aegis::Vec4(0,0,0,1));
 }
 
