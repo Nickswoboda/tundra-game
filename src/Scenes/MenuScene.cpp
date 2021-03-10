234e797b076e4c57892440aadfff438ab4a99536
Nick Swoboda <nickswoboda@gmail.com>
2021-03-09 21:26:09 -0800

    Refactor: Add StylizeButton function
    Due to the changes in Aegis for button customization. I can now create
    decent looking buttons without the need of textures. The StylizeButton
    func also allows to reduce redundant code since all buttons share most
    features.

--- src/Scenes/MenuScene.cpp
+++ src/Scenes/MenuScene.cpp
@@ -7,6 +7,19 @@
 
 #include <iostream>
 
+void StylizeButton(Aegis::Button& button, int font_size)
+{
+	button.SetStateBgColor(Aegis::Button::Normal, { 0.37f, 0.5f,  0.72f, 1.0f });
+	button.SetStateBgColor(Aegis::Button::Pressed, { 0.37f, 0.5f,  0.72f, 1.0f });
+	button.SetStateBgColor(Aegis::Button::Hovered, { 0.45f, 0.58f, 0.78f, 1.0f });
+	button.border_size_ = 3;
+
+	button.SetFont(Aegis::FontManager::Load("assets/fonts/roboto_bold.ttf", font_size));
+
+	auto hover_sfx = Aegis::AudioPlayer::LoadSound("assets/audio/button_hover.ogg");
+	button.ConnectSignal("entered", [hover_sfx]() {Aegis::AudioPlayer::PlaySound(hover_sfx, 40); });
+}
+
 MenuScene::MenuScene(GameData& game_data) 
 	:game_data_(game_data)
 {
@@ -18,20 +31,20 @@ MenuScene::MenuScene(GameData& game_data)
 	auto title_texture = Aegis::Texture::Create("assets/textures/title.png");
 	auto title_sprite = v_box_->AddWidget<Aegis::SpriteWidget>(Aegis::Vec2(0, 50), title_texture);
 
-	auto new_game_button = v_box_->AddWidget<Aegis::Button>(Aegis::AABB( 580, 170, 200, 50 ));
-	auto level_select_button = v_box_->AddWidget<Aegis::Button>(Aegis::AABB( 580, 220, 200, 50 ));
-	auto options_button = v_box_->AddWidget<Aegis::Button>(Aegis::AABB( 580, 230, 200, 50 ));
-	auto exit_button = v_box_->AddWidget<Aegis::Button>(Aegis::AABB( 580, 270, 200, 50 ));
+	auto new_game_button = v_box_->AddWidget<Aegis::Button>(Aegis::AABB( 580, 170, 200, 50 ), "PLAY");
+	auto level_select_button = v_box_->AddWidget<Aegis::Button>(Aegis::AABB( 580, 220, 200, 50 ), "LEVEL SELECT");
+	auto options_button = v_box_->AddWidget<Aegis::Button>(Aegis::AABB( 580, 230, 200, 50 ), "OPTIONS");
+	auto exit_button = v_box_->AddWidget<Aegis::Button>(Aegis::AABB( 580, 270, 200, 50 ), "EXIT");
+
+	StylizeButton(*new_game_button, 32);
+	StylizeButton(*level_select_button, 28);
+	StylizeButton(*options_button, 32);
+	StylizeButton(*exit_button, 32);
 
 	new_game_button->ConnectSignal("pressed", [&]() {manager_->PushScene<GameplayScene>(1, game_data_); Aegis::AudioPlayer::StopSound(bg_music_); });
 	level_select_button->ConnectSignal("pressed", [&]() {manager_->PushScene<LevelSelectScene>(game_data_);});
 	options_button->ConnectSignal("pressed", [&]() {manager_->PushScene<OptionsScene>();});
 	exit_button->ConnectSignal("pressed", []() { Aegis::Application::Quit();});
-
-	new_game_button->ConnectSignal("entered", [&]() {Aegis::AudioPlayer::PlaySound(button_hover_sfx_, 40); });
-	level_select_button->ConnectSignal("entered", [&]() {Aegis::AudioPlayer::PlaySound(button_hover_sfx_, 40); });
-	options_button->ConnectSignal("entered", [&]() {Aegis::AudioPlayer::PlaySound(button_hover_sfx_, 40); });
-	exit_button->ConnectSignal("entered", [&]() {Aegis::AudioPlayer::PlaySound(button_hover_sfx_, 40); });
 
 	auto mute_button = ui_layer_->AddWidget<Aegis::Checkbox>("", Aegis::AABB(1200, 650, 50, 50));
 	mute_button->ConnectSignal("checked", [](){Aegis::AudioPlayer::SetMasterVolume(0);});
@@ -39,17 +52,7 @@ MenuScene::MenuScene(GameData& game_data)
 	mute_button->SetTexture(false, Aegis::TextureManager::Load("assets/textures/audio_on.png"));
 	mute_button->SetTexture(true, Aegis::TextureManager::Load("assets/textures/audio_off.png"));
 	
-	new_game_button->SetStateTexture(Aegis::Button::Normal, Aegis::TextureManager::Load("assets/textures/play_btn.png"));
-	new_game_button->SetStateTexture(Aegis::Button::Hovered, Aegis::TextureManager::Load("assets/textures/play_btn_hovered.png"));
-	level_select_button->SetStateTexture(Aegis::Button::Normal, Aegis::TextureManager::Load("assets/textures/level_select_btn.png"));
-	level_select_button->SetStateTexture(Aegis::Button::Hovered, Aegis::TextureManager::Load("assets/textures/level_select_btn_hovered.png"));
-	options_button->SetStateTexture(Aegis::Button::Normal, Aegis::TextureManager::Load("assets/textures/options_btn.png"));
-	options_button->SetStateTexture(Aegis::Button::Hovered, Aegis::TextureManager::Load("assets/textures/options_btn_hovered.png"));
-	exit_button->SetStateTexture(Aegis::Button::Normal, Aegis::TextureManager::Load("assets/textures/exit_btn.png"));
-	exit_button->SetStateTexture(Aegis::Button::Hovered, Aegis::TextureManager::Load("assets/textures/exit_btn_hovered.png"));
-	
 	snow_engine_ = std::make_unique<ParticleEngine>(512, Aegis::Vec2(-200, -700), Aegis::Vec2(Aegis::Application::GetWindow().GetSize().x, 0), Aegis::Vec2(0.2, 2), Aegis::Vec2(1, 3));
-	button_hover_sfx_ = Aegis::AudioPlayer::LoadSound("assets/audio/button_hover.ogg");
 	bg_music_ = Aegis::AudioPlayer::LoadSound("assets/audio/menu_bgm.ogg", true, true);
 	Aegis::AudioPlayer::PlaySound(bg_music_, 100);
 }
